9bad25ff3fb804e4fdacddd9a131652957ff002b
Florian Schulz <florian.schulz404@gmail.com>
2024-07-21 18:44:29 +0200

    📝 added readme & credits

---
+++ README.md
@@ -0,0 +1,46 @@
+# AciiFX
+
+> Simple & Powerful Aciiverse NodeTS Backend
+> [Here](https://) is the doc also in english.
+
+## Entwickler*Innen
+
+- [Flowtastisch](https://)
+
+## Credits
+
+- [cors](https://github.com/expressjs/cors)
+- [dotenv](https://github.com/motdotla/dotenv)
+- [express](https://github.com/expressjs/express)
+- [javascript](https://www.javascript.com/)
+- [jsonwebtoken](https://github.com/auth0/node-jsonwebtoken)
+- [typescript](https://github.com/Microsoft/TypeScript)
+
+## Features
+
+- 
+
+## Lokaler Setup (DEV)
+
+### Was du brauchst (Lokaler Setup)
+
+- NodeJS (>= vXX)
+- TypeScript Modul
+- Erreichbare MySQL Datenbank
+
+### Schritt für Schritt (Lokaler Setup)
+
+1. So setzt du das alles auf
+2. Dann das hier
+
+## Server Setup
+
+### Was du brauchst (Server Setup)
+
+- Erreichbare MySQL Datenbank
+- Server (Wir nutzen die Debian 12 Architektur)
+
+### Schritt für Schritt (Server Setup)
+
+1. So setzt du das alles auf
+2. Dann das hier
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,64 @@
 import express  = require('express');
 import cors     = require('cors');
 
-// Read environment from direction "./"
+// Read environments
+/**
+ * @Copyright (c) 2015, Scott Motte
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 require('dotenv').config();
 
 // Get env data
 const   port    = parseInt(process.env.FX_PORT!) || 443,
         system  = process.env.FX_SYSTEM || "LOCAL",
-        domain  = process.env.FX_DOMAIN || "",
-        app     = express(),
+        domain  = process.env.FX_DOMAIN || "";
+
+/**
+ * @Copyright (MIT Licence) (c) 2009-2014 TJ Holowaychuk <tj@vision-media.ca>
+ * @Copyright (MIT License) (c) 2013-2014 Roman Shtylman <shtylman+expressjs@gmail.com>
+ * @Copyright (MIT License) (c) 2014-2015 Douglas Christopher Wilson <doug@somethingdoug.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * 'Software'), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+const   app     = express(),
         router  = require('./lib/router');
 
 let serviceURL: string;
@@ -29,7 +79,31 @@ process.on('uncaughtException', function (err) {
 });
 
 app.use(express.json());
+
+/**
+ * @Copyright (MIT Licence) (c) 2013 Troy Goode <troygoode@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * 'Software'), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 app.use(cors());
+
 app.use('/api', router);
 
 app.listen(port, () => {
